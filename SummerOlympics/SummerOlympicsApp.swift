d7ea153e0d7735fb9cd21348b2c7d6e3df222355
Paul Wagstaff <paul@pwfotos.ca>
2025-11-24 10:45:04 -0700

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by Paul Wagstaff on 2025-11-24.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
